248e7e52ae210b7610305daf8c43b9a7c91e4920
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-26 21:41:46 +0700

    Python Constants

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 if __name__ == "__main__":
-  print("Python Constants")
+  # Python Constants
+
+  """
+  The bad new is Python doesn'tsupport constants.
+
+  To work around this, you use all capital letters to name a variable to indicate that the variable should be treated as a constants.
+  For example:
+  """
+
+  FILE_SIZE_LIMIT = 2_000
+  print(FILE_SIZE_LIMIT)
